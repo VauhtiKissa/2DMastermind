4e5fc865ea3d50cb71f4aae7140632b8abafd11c
VauhtiKissa <tuisku.junkkala@gmail.com>
2024-05-18 20:23:21 +0300

    loss screen restarts music level

--- code/loss_screen.cs
+++ code/loss_screen.cs
@@ -4,16 +4,20 @@ using System;
 public partial class loss_screen : Node2D
 {
 	public void back_to_menu(){
+
+		music_player music_box = (music_player)GetTree().Root.GetChild(0);
+		music_box.restart_sound_level();
+
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/menu.tscn").Instantiate());
 		GetParent().GetParent().QueueFree();
 	}
 
 	public void restart(){
+
+		music_player music_box = (music_player)GetTree().Root.GetChild(0);
+		music_box.restart_sound_level();
+
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/game.tscn").Instantiate());
 		GetParent().QueueFree();
-	}
-	
-	public void quit(){
-		GetTree().Quit();
 	}
 }
