315d5fcf6c4ab7543da715654a9b7e2ab5925601
VauhtiKissa <tuisku.junkkala@gmail.com>
2024-05-19 13:13:50 +0300

    made menu background move instead of the camera(for mute buttons)

--- code/grid_maker.cs
+++ code/grid_maker.cs
@@ -1,11 +1,19 @@
 using Godot;
 using System;
+using System.Data;
 
 public partial class grid_maker : Node2D
 {
 	/*
 		makes the background for the main menu
 	*/
+	private double circle_poss;
+	[Export]
+	private float circle_distance = 5;
+	[Export]
+	private float circle_speed = 1;
+
+
 	private TileMap tile_map;
 	public override void _Ready()
 	{
@@ -18,5 +26,11 @@ public partial class grid_maker : Node2D
 				tile_map.SetCell(0,new Vector2I(i-15,e-11),0,new Vector2I(GD.RandRange(0,1),GD.RandRange(0,3)),0);
 			}
 		}
+	}
+
+	public override void _Process(double delta)
+	{
+		circle_poss = (circle_poss + delta * circle_speed)%(Math.PI*2);
+		tile_map.Position = new Vector2((float)Math.Sin(circle_poss),(float)Math.Cos(circle_poss)) * circle_distance;
 	}
 }
--- code/menu.cs
+++ code/menu.cs
@@ -8,12 +8,6 @@ public partial class menu : Node2D
 	public Node normal_game;
 	public Node tutorial_game;
 
-	private double circle_poss;
-	[Export]
-	private float circle_distance = 5;
-	[Export]
-	private float circle_speed = 1;
-
 	public override void _Ready()
 	{
 		normal_game = ResourceLoader.Load<PackedScene>("res://prefabs/game/game.tscn").Instantiate();
@@ -23,12 +17,6 @@ public partial class menu : Node2D
 		((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(3), true);
 		((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(4), true);
 
-	}
-
-	public override void _Process(double delta)
-	{
-		circle_poss = (circle_poss + delta * circle_speed)%(Math.PI*2);
-		Position = new Vector2((float)Math.Sin(circle_poss),(float)Math.Cos(circle_poss)) * circle_distance;
 	}
 
 	public void play_pressed(){
