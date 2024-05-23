2e4939fefc5ba81d60eba13d61cc54251e63fc4a
VauhtiKissa <tuisku.junkkala@gmail.com>
2024-05-23 22:34:42 +0300

    added fullscreen button and moved other around

--- code/config_manager.cs
+++ code/config_manager.cs
@@ -9,18 +9,21 @@ public class Config{
 	public bool music_mute { get; set;} = false;
 
 	public bool button_mute { get; set;} = false;
+	public bool Fullscreen { get; set;} = false;
 }
 
 public partial class config_manager : Node
 {
 
-	// Called when the node enters the scene tree for the first time.
+	private TextureButton mute_button;
+
 	public override void _Ready()
 	{
 		if(Godot.FileAccess.FileExists("user://2DMastermind.json")){
     	Godot.FileAccess file = Godot.FileAccess.Open("user://2DMastermind.json", Godot.FileAccess.ModeFlags.Read);
 		config = JsonSerializer.Deserialize<Config>(file.GetAsText());
     	file.Close();
+		mute_button = (TextureButton)GetChild(0);
 		}else{
 			config = new Config();
 		}
@@ -33,4 +36,16 @@ public partial class config_manager : Node
 		file.Close();
 	}
 	public static Config config;
+
+	public void change_full_screen(){
+		config.Fullscreen = !config.Fullscreen;
+		if(config.Fullscreen){
+			GetWindow().Mode = Window.ModeEnum.Fullscreen;
+			mute_button.TextureNormal = (Texture2D)GD.Load("res://sprites/other_buttons/fullscreen_off.png");
+		}else{
+			GetWindow().Mode = Window.ModeEnum.Maximized;
+			mute_button.TextureNormal = (Texture2D)GD.Load("res://sprites/other_buttons/fullscreen_on.png");
+		}
+
+	}
 }
