8e771c4d1a68a98330c61ddecd64a6ad0d0f8335
VauhtiKissa <tuisku.junkkala@gmail.com>
2024-05-23 23:20:23 +0300

    fixed loss and victory screen music reset

--- code/loss_screen.cs
+++ code/loss_screen.cs
@@ -5,25 +5,21 @@ public partial class loss_screen : Node2D
 {
 	public override void _Ready()
 	{
-        ((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(2), true);
-        ((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(3), true);
+        ((button_sound)GetNode("/root/ButtonSoundMaker")).connect_button((TextureButton)GetChild(2), false);
+        	((button_sound)GetNode("/root/ButtonSoundMaker")).connect_button((TextureButton)GetChild(3), false);
 
 		((AnimationPlayer)GetChild(4)).Active = true;
 		((AnimationPlayer)GetChild(4)).Play("loss_pop_up");
 	}
 
 	public void back_to_menu(){
-
-		((music_player)GetTree().Root.GetChild(0)).restart_sound_level();
-
+		((music_player)GetNode("/root/MusicPlayer")).restart_sound_level();
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/menu.tscn").Instantiate());
 		GetParent().GetParent().QueueFree();
 	}
 
 	public void restart(){
-		
-		((music_player)GetTree().Root.GetChild(0)).restart_sound_level();
-
+		((music_player)GetNode("/root/MusicPlayer")).restart_sound_level();
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/game.tscn").Instantiate());
 		GetParent().GetParent().QueueFree();
 	}
--- code/victory_screen.cs
+++ code/victory_screen.cs
@@ -6,25 +6,21 @@ public partial class victory_screen : Node2D
 
 	public override void _Ready()
 	{
-        ((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(2), true);
-        ((button_sound)GetTree().Root.GetChild(1)).connect_button((TextureButton)GetChild(3), true);
+        ((button_sound)GetNode("/root/ButtonSoundMaker")).connect_button((TextureButton)GetChild(2), false);
+        ((button_sound)GetNode("/root/ButtonSoundMaker")).connect_button((TextureButton)GetChild(3), false);
 
 		((AnimationPlayer)GetChild(4)).Active = true;
 		((AnimationPlayer)GetChild(4)).Play("victory_animation");
 	}
 
 	public void back_to_menu(){
-
-		((music_player)GetTree().Root.GetChild(0)).restart_sound_level();
-
+		((music_player)GetNode("/root/MusicPlayer")).restart_sound_level();
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/menu.tscn").Instantiate());
 		GetParent().GetParent().QueueFree();
 	}
 
 	public void restart(){
-
-		((music_player)GetTree().Root.GetChild(0)).restart_sound_level();
-
+		((music_player)GetNode("/root/MusicPlayer")).restart_sound_level();
 		GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://prefabs/game/game.tscn").Instantiate());
 		GetParent().GetParent().QueueFree();
 	}
